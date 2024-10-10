ef47e1a0d0cdcf23d3dea74ec300860c43418db0
Sarahsarahshahab12345 <sarahshahab2006@gmail.com>
2024-10-11 02:05:24 +0500

    Nike HTML CSS React

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <nav className="container">
+        <div className="logo">
+          <img src="/images/logo.png" alt="Logo" />
+        </div>
+
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
